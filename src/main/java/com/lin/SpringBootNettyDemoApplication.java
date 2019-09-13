b1c116ad97e31d7726545c892de001c20f4cc279
lkmc2 <lkmc2@163.com>
2019-09-13 13:51:20 +0800

    添加配置属性

--- src/main/java/com/lin/SpringBootNettyDemoApplication.java
+++ src/main/java/com/lin/SpringBootNettyDemoApplication.java
@@ -3,6 +3,11 @@ package com.lin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 应用启动类
+ * @author lkmc2
+ * @date 2019-09-13 13:47
+ */
 @SpringBootApplication
 public class SpringBootNettyDemoApplication {
 
